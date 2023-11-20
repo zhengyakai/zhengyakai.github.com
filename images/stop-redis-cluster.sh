06d7b189299d744666c5cd2987c2af09fd3530ef
zhengyk <zhengyakai@aliyun.com>
2023-11-20 13:01:46 +0800

    Site updated: 2023-11-20 13:01:46

--- images/stop-redis-cluster.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-docker stop redis-7007 redis-7002 redis-7003 redis-7004 redis-7005 redis-7006
-
-docker rm redis-7007 redis-7002 redis-7003 redis-7004 redis-7005 redis-7006
-
-rm -rf 7007 7002 7003 7004 7005 7006
